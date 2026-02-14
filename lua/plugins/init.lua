eb0a7e5a52f082a2e5b19d6fff43831b8c258763
Sabin Koirala <146768657+skoirala0391@users.noreply.github.com>
2026-02-13 23:05:24 -0500

    add: commenting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,25 @@ return {
     "mfussenegger/nvim-jdtls",
   },
   {
+    "numToStr/Comment.nvim",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    opts = function()
+      return {
+        pre_hook = require(
+          "ts_context_commentstring.integrations.comment_nvim"
+        ).create_pre_hook(),
+      }
+    end,
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    opts = {
+      enable_autocmd = false,
+    },
+  },
+  {
     "mrcjkb/rustaceanvim",
     version = "^5",
     ft = { "rust" },
